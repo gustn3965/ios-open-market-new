fb9f034a6130088339456e33fb83aba32fb79f19
vapor.park <vapor.park@kakaocorp.com>
2022-01-26 13:52:54 +0900

    viewController 수정

--- OpenMarket/OpenMarket/ViewController/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("?????")
         // Do any additional setup after loading the view.
     }
 }
